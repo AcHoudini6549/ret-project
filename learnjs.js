91bc41d050bdd6f089c77734005b528e00e3f88e
bowden <houdini6549@gmail.com>
2019-10-29 15:36:25 +1100

    fixed render issue canvas and red box now appear

--- learnjs.js
+++ learnjs.js
@@ -1,18 +1,18 @@
 var myGamePiece;
 
 function startGame() {
-    myGameArea.start();
-    myGamePiece = new CompositionEvent (30, 30, "red", 10, 120);
-  }
-  
-  function component(width, height, color, x, y,) {
-      this.width = width;
-      this.height = height;
-      this.x = x;
-      this.y = y;
-      ctx = myGameArea.context;
-      ctx.fillStyle = color;
-      ctx.fillRect(this.x, this.y, this.width, this.height,);
+  myGameArea.start();
+  myGamePiece = new component(30, 30, "red", 10, 120);
+}
+
+function component(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  ctx = myGameArea.context;
+  ctx.fillStyle = color;
+  ctx.fillRect(this.x, this.y, this.width, this.height);
   }
   var myGameArea = {
     canvas : document.createElement("canvas"),
