5c915fa6fc368f0103fcbc7e389be1b56014c11b
bowden <houdini6549@gmail.com>
2019-10-30 10:24:52 +1100

    added frame instancing and cleasr previous frames

--- learnjs.js
+++ learnjs.js
@@ -14,6 +14,7 @@ function component(width, height, color, x, y) {
   ctx.fillStyle = color;
   ctx.fillRect(this.x, this.y, this.width, this.height);
   }
+
   var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
@@ -21,6 +22,25 @@ function component(width, height, color, x, y) {
       this.canvas.height = 270;
       this.context = this.canvas.getContext("2d");
       document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    },
+    clear : function() {
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
   }
 
+  function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.update = function(){
+      ctx = myGameArea.context;
+      ctx.fillStyle = color;
+      ctx.fillRect(thius.x, this.y, this.width, this.height);
+    }
+  }
+
+  function updateGameArea() {
+    myGameArea.clear();
+    myGameArea.update();
+  }
