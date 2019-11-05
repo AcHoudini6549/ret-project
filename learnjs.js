94358021424a90647c9e9e5395b33fa6a3232dce
bowden <houdini6549@gmail.com>
2019-11-05 12:33:16 +1100

    broken updater.game.area function

--- learnjs.js
+++ learnjs.js
@@ -37,36 +37,36 @@ function component(width, height, color, x, y) {
     this.speedX = 0;
     this.speedY = 0;
     this.x = x;
-    this.y = y;
-    this.update = function(){
-      ctx = myGameArea.context;
-      ctx.fillStyle = color;
-      ctx.fillRect(this.x, this.y, this.width, this.height);
+    this.y = y;    
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-    this.newPos = function(){
-      this.x += this.speedX;
-      this.y += this.speedY;
-    }
-  }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;        
+    }    
+}
 
-  function updateGameArea() {
+function updateGameArea() {
     myGameArea.clear();
-    myGamerPiece.newPos();
+    myGamePiece.newPos();
     myGamePiece.update();
-  }
+}
 
-  function moveup(){
-    myGamePiece.speedY -= 1;
-  }
-  
-  function movedown() {
-    myGamePiece.speedY -= 1;
-  }
+function moveup() {
+    myGamePiece.speedY -= 1; 
+}
+
+function movedown() {
+    myGamePiece.speedY += 1; 
+}
 
 function moveleft() {
-  myGamePiece.speedX -= 1;
+    myGamePiece.speedX -= 1; 
 }
 
 function moveright() {
-  myGamePiece.speedX -= 1;
+    myGamePiece.speedX += 1; 
 }
